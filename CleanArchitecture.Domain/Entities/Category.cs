e0c9ae5ce8bd296add038f1e2697e948c0065f0e
Mateus Nadal <nadalmateus@hotmail.com>
2022-04-25 17:23:22 -0300

    feat: project setup + entities

---
+++ CleanArchitecture.Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchitecture.Domain.Entities
+{
+    class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ CleanArchitecture.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CleanArchitecture.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
