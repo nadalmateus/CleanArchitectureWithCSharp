fd497e2ff7c0754220f513a49cdb4ff9dfc50a5e
Mateus Nadal <nadalmateus@hotmail.com>
2022-04-25 21:39:28 -0300

    feat: added validation to Category and Product entity

---
+++ CleanArchitecture.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> GetCategoryByIdAync(int? id);
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> CreateCategoryAsync(Category category);
+        Task<Category> UpdateCategoryAsync(Category category);
+        Task<Category> DeleteCategoryAsync(Category category);
+    }
+}
---
+++ CleanArchitecture.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAync(int? id);
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Category> CreateProductAsync(Product product);
+        Task<Category> UpdateProductAsync(Product product);
+        Task<Category> DeleteProductAsync(Product product);
+    }
+}
