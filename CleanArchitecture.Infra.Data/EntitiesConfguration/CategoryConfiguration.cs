ad60649b254f0c70b8369c263c9e6ab45f86983e
Mateus Nadal <nadalmateus@hotmail.com>
2022-04-26 22:21:16 -0300

    feat: added ef fluent mappings

---
+++ CleanArchitecture.Infra.Data/EntitiesConfguration/CategoryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Infra.Data.EntitiesConfguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(id => id.Id);
+            builder.Property(name => name.Name).HasMaxLength(100).IsRequired();
+
+            builder.HasData(new Category(1, "Software"), new Category(2, "Devices"), new Category(3, "Courses"));
+        }
+    }
+}
