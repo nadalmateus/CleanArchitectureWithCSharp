76bcbc2337da3771c266452403947fa4e6b7301f
Mateus Nadal <nadalmateus@hotmail.com>
2022-04-28 21:16:31 -0300

    feat: added a test view

---
+++ CleanArchitecture.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Application.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetCategoriesAsync();
+            return View(categories);
+        }
+    }
+}
