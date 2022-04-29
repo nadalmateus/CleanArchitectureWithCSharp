6fc5f86ae037204e9966941d3afb6d75406a03d5
Mateus Nadal <nadalmateus@hotmail.com>
2022-04-28 22:32:48 -0300

    feat: added cqrs queries

---
+++ CleanArchitecture.Application/Products/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+using MediatR;
+
+namespace CleanArchitecture.Application.Products.Queries
+{
+    public class GetProductByIdQuery : IRequest<Product>
+    {
+        public GetProductByIdQuery(int id)
+        {
+            Id = Id;
+        }
+
+        public int Id { get; set; }
+    }
+}
