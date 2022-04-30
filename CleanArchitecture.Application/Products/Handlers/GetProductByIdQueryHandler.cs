4b798fa0d1a2fbcf705eccdebfeacf0ad93c10d0
Mateus Nadal <nadalmateus@hotmail.com>
2022-04-29 21:02:23 -0300

    feat: added get by id and unique get handler for product entity

---
+++ CleanArchitecture.Application/Products/Handlers/GetProductByIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿
+using CleanArchitecture.Application.Products.Queries;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces;
+
+using MediatR;
+
+namespace CleanArchitecture.Application.Products.Handlers
+{
+    internal class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Product>
+    {
+        private readonly IProductRepository _productRepository;
+        public GetProductByIdQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetProductByIdAync(request.Id);
+        }
+    }
+}
