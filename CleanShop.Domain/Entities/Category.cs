802e73b9e8784b307c41cc6777220e50c7225a5d
Mateus Nadal <nadalmateus@hotmail.com>
2022-07-27 12:37:26 -0300

    feat: enriched the created domain model, and created a validation class

--- CleanShop.Domain/Entities/Category.cs
+++ CleanShop.Domain/Entities/Category.cs
@@ -1,8 +1,28 @@
+using CleanShop.Domain.Validation;
+
 namespace CleanShop.Domain.Entities;
-public class Category
+public sealed class Category
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
+    public Category(string name)
+    {
+        ValidateDomain(name);
+    }
+    public Category(int id, string name)
+    {
+        DomainExceptionValidation.When(id < 0, "Invalid ID value");
+        ValidateDomain(name);
+    }
+
+    public int Id { get; private set; }
+    public string Name { get; private set; }
 
     public ICollection<Product> Products { get; set; }
+
+    private void ValidateDomain(string name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.Name is required");
+        DomainExceptionValidation.When(name.Length < 3, "Invalid name, to short, minmum 3 characters");
+
+        Name = name;
+    }
 }
