8086d31031a0531165efe6625e35a788b5a02ec5
Mateus Nadal <nadalmateus@hotmail.com>
2022-07-27 12:21:47 -0300

    feat: creating the domain model entities

--- CleanShop.Domain/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace CleanShop.Domain;
-public class Class1
-{
-
-}
---
+++ CleanShop.Domain/Entities/Category.cs
@@ -0,0 +1,8 @@
+namespace CleanShop.Domain.Entities;
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public ICollection<Product> Products { get; set; }
+}
