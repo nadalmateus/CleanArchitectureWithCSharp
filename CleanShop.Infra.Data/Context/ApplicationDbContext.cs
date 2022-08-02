6adac167602003cb3dd9c8770135967f4014c39f
Mateus Nadal <nadalmateus@hotmail.com>
2022-08-01 22:28:30 -0300

    feat: configuring entities using the Fluent API

--- CleanShop.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanShop.Infra.Data/Context/ApplicationDbContext.cs
@@ -6,10 +6,7 @@ namespace CleanShop.Infra.Data.Context;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
