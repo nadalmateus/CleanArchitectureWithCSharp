712d076376a5a449beec0581163cb5ce5efd603b
Mateus Nadal <nadalmateus@hotmail.com>
2022-08-05 11:15:49 -0300

    feat: added auto mapper in application project

---
+++ CleanShop.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using CleanShop.Application.DTO;
+
+namespace CleanShop.Application.Interfaces;
+public interface ICategoryService
+{
+    Task<IEnumerable<CategoryDTO>> GetCategories();
+    Task<CategoryDTO> GetCategoryById(int? id);
+    Task Add(CategoryDTO categoryDTO);
+    Task Update(CategoryDTO categoryDTO);
+    Task Delete(int? id);
+}
---
+++ CleanShop.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+using CleanShop.Application.DTO;
+using CleanShop.Domain.Entities;
+
+namespace CleanShop.Application.Mappings;
+public class DomainToDTOMappingProfile : Profile
+{
+    public DomainToDTOMappingProfile()
+    {
+        CreateMap<Category, CategoryDTO>().ReverseMap();
+        CreateMap<Product, ProductDTO>().ReverseMap();
+    }
+}
